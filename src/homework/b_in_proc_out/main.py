268515f08ea59b2f8f6e5924bdac4f5c18bec9ab
miacdelgado <153124560+miacdelgado@users.noreply.github.com>
2024-01-27 23:29:46 +0000

    Homework 1

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,15 @@
-#
+# This program calls the multiply_numbers function defined in the
+# output.py file
+import output
+
+# This runs the numbers 6 and 6 through the function
+result = output.multiply_numbers(6, 6)
+print(result)
+
+
+# This asks the user for two numbers and then runs them through
+# the multiply numbers function
+num1 = int(input('Please enter a number: '))
+num2 = int(input('Please enter a second number: '))
+result = output.multiply_numbers(num1, num2)
+print(num1, '*', num2, '=', result)
--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -4,15 +4,13 @@ import unittest
 The file at /src/homework/b_in_proc_out/output has 
 the function get_number.
 '''
-from src.homework.b_in_proc_out.output import get_number
+from src.homework.b_in_proc_out.output import get_number, multiply_numbers
 
 class Test_Config(unittest.TestCase):
+    self.assertEqual(2, get_number(2))
 
-    def test_get_number_1(self):
-        #test that the function get_number returns 1
-        self.assertEqual(1, get_number(1))
+    def test_multiply_numbers_1(self):
+        self.assertEqual(multiply_numbers(5, 5), 25)
     
-    def test_get_number_2(self):
-        #test that the function get_number returns 2
-        self.assertEqual(2, get_number(2))
-
+    def test_multiply_numbers_2(self):
+        self.assertEqual(multiply_numbers(10, 10), 100)
