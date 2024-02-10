9a29495108e779eaa917090d90fc8fcea1bb473b
miacdelgado <153124560+miacdelgado@users.noreply.github.com>
2024-02-10 23:32:13 +0000

    Fixing homework 1 test cases

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,18 @@
 import unittest
-'''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
-has the test functions
-'''
-from tests.homework.b_in_proc_out import tests_in_proc_out
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
-unittest.TextTestRunner(verbosity=2).run(suite)
+from src.homework.b_in_proc_out.output import get_number, multiply_numbers
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_number_1(self):
+        self.assertEqual(1, get_number(1))
+
+    def test_get_number_2(self):
+        self.assertEqual(2, get_number(2))
+
+    def test_multiple_numbers_1(self):
+        self.assertEqual(multiply_numbers(5,5), 25)
+
+    def test_multiple_numbers_2(self):
+        self.assertEqual(multiply_numbers(10,10), 100)
+        
