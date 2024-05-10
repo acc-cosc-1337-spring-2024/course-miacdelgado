770f595245e1fa5de2fdce9cde5ffb5fee8351dd
miacdelgado <153124560+miacdelgado@users.noreply.github.com>
2024-05-10 03:42:27 +0000

    Homework 9

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tests.homework.i_dictionaries_sets import tests_dictionaries_and_sets
+from tests.homework.j_classes import tests_classes
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
+suite = unittest.TestLoader().loadTestsFromModule(tests_classes)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,17 @@
-#
+import unittest
+
+from src.homework.j_classes.class_a import roll, get_rolled_value
+
+class Test_Config(unittest.TestCase):
+    def test_roll_1(self):
+        self.assertEqual(roll(1), 1)
+    def test_roll_2(self):
+        self.assertEqual(roll(2), 2)
+    def test_roll_3(self):
+        self.assertEqual(roll(3), 3)
+    def test_roll_4(self):
+        self.assertEqual(roll(4), 4)
+    def test_roll_5(self):
+        self.assertEqual(roll(5), 5)
+    def test_roll_6(self):
+        self.assertEqual(roll(6), 6)
